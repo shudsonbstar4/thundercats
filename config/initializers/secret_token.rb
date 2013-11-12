c77599ba55d306c1c7990e86c10364489ea5370a
Sarah Hudson <sarah@sarah-hudson.com>
2013-11-12 08:21:01 -0500

    Initial Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thundercats::Application.config.secret_token = 'b234e0349c0301c0f225e27a5c514e1f4b8ee64750cd68e120cb1fff0bcb2a6f66ef958ffdd768efc263d2dbdd1452d84f738afd4a5c7af56d7c61c0f590c4de'
